e37adf0545ae718261043c9a9c9be9651a712541
Mike Bommarito <michael.bommarito@gmail.com>
2025-01-05 11:48:44 -0500

    relaxed dep versions and bump package version

--- alea_data_generator/__init__.py
+++ alea_data_generator/__init__.py
@@ -2,5 +2,5 @@
 alea_data_generator is a Python package for generating synthetic data for machine learning tasks.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "ALEA Institute (https://aleainstitute.ai/)"
